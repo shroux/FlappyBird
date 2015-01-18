025f665f23f4705e83fa7182c336bdc7302313ae
Gilles Tual <gilles.tual@epitech.eu>
2015-01-18 17:40:19 +0800

    Added title on MainMenu screen

--- js/main.js
+++ js/main.js
@@ -24,6 +24,21 @@ FlappyBirdReborn.MainMenu.prototype = {
 
     create: function(){
         this.background = this.game.add.sprite(0, 0, "background");
+
+        this.ground = this.game.add.tileSprite(0, 400, 335, 112, 'ground');
+        this.ground.autoScroll(-200, 0);
+
+        this.titleGroup = this.game.add.group();
+        this.title = this.game.add.sprite(0, 0, "title");
+        this.titleGroup.add(this.title);
+
+        this.bird = this.game.add.sprite(200, 5, "bird");
+        this.titleGroup.add(this.bird);
+        this.bird.animations.add("flap");
+        this.bird.animations.play("flap", 12, true);
+
+        this.titleGroup.x = 30;
+        this.titleGroup.y = 100;
     },
     update: function(){
     }
