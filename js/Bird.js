0b3e4615acf99263bc58913e0d3e87f81d87c902
Gilles Tual <gilles.tual@epitech.eu>
2015-01-19 14:29:16 +0800

    added play screen with bird + ground

---
+++ js/Bird.js
@@ -0,0 +1,16 @@
+    var Bird = function(game, x, y, frame){
+        Phaser.Sprite.call(this, game, x, y, 'bird', frame);
+        this.anchor.setTo(0.5, 0.5);
+
+        this.animations.add('flap');
+        this.animations.play('flap', 12, true);
+
+        this.game.physics.arcade.enableBody(this);
+    };
+
+    Bird.prototype = Object.create(Phaser.Sprite.prototype);
+    Bird.prototype.constructor = Bird;
+
+    Bird.prototype.update = function() {
+
+    };
--- js/main.js
+++ js/main.js
@@ -57,8 +57,19 @@ FlappyBirdReborn.Play.prototype = {
     },
 
     create: function(){
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.game.physics.arcade.gravity.y = 500;
+
+        this.background = this.game.add.sprite(0, 0, 'background');
+
+        this.bird = new Bird(this.game, 100, 505/2);
+        this.game.add.existing(this.bird);
+
+        this.ground = new Ground(this.game, 0, 400, 335, 112);
+        this.game.add.existing(this.ground);
     },
     update: function(){
+        this.game.physics.arcade.collide(this.bird, this.ground);
     }
 }
 
